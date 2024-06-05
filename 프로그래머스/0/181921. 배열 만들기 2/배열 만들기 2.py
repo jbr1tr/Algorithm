e0d649fe3bb6458fc8f15b4e55288ff9057db761
Jiwoo Hwang <63641939+jbr1tr@users.noreply.github.com>
2024-06-05 10:46:01 +0900

    Update 배열 만들기 2.py

--- 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.py
+++ 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.py
@@ -2,6 +2,7 @@ def solution(l, r):
     answer = []
     arr = ["1", "2", "3", "4", "6", "7", "8", "9"]
     cnt = 0
+    
     for i in range(l, r+1):
         for j in str(i):
             if j in arr:
@@ -9,6 +10,8 @@ def solution(l, r):
         if cnt == 0:
             answer.append(i)
         cnt = 0
+        
     if len(answer) == 0:
         answer.append(-1)
+        
     return answer
