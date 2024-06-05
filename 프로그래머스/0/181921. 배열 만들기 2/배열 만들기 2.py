40f8e17a71f273ed97a86e91b955edb6d98692a3
Jiwoo Hwang <63641939+jbr1tr@users.noreply.github.com>
2024-06-05 10:43:44 +0900

    [level 0] Title: 배열 만들기 2, Time: 33.09 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.py
@@ -0,0 +1,14 @@
+def solution(l, r):
+    answer = []
+    arr = ["1", "2", "3", "4", "6", "7", "8", "9"]
+    cnt = 0
+    for i in range(l, r+1):
+        for j in str(i):
+            if j in arr:
+                cnt += 1
+        if cnt == 0:
+            answer.append(i)
+        cnt = 0
+    if len(answer) == 0:
+        answer.append(-1)
+    return answer
