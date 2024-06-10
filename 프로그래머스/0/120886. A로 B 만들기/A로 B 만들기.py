0ebfa23a44522fa525a337ad5eb1c5465b0c8d86
Jiwoo Hwang <63641939+jbr1tr@users.noreply.github.com>
2024-06-10 17:17:27 +0900

    [level 0] Title: A로 B 만들기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120886. A로 B 만들기/A로 B 만들기.py
+++ 프로그래머스/0/120886. A로 B 만들기/A로 B 만들기.py
@@ -1,9 +1,10 @@
 def solution(before, after):
     answer = 0
     
-    before = sorted(list(before))
-    after = sorted(list(after))
+    before = sorted(before)
+    after = sorted(after)
     
     if before == after:
         answer = 1
+        
     return answer
