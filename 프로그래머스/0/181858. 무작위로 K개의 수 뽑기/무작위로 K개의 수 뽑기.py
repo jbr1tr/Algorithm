c85aec47b275da76528040efe8a0e8e5dc8cd57c
Jiwoo Hwang <63641939+jbr1tr@users.noreply.github.com>
2024-06-10 12:15:01 +0900

    [level 0] Title: 무작위로 K개의 수 뽑기, Time: 342.29 ms, Memory: 10.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181858. 무작위로 K개의 수 뽑기/무작위로 K개의 수 뽑기.py
@@ -0,0 +1,12 @@
+def solution(arr, k):
+    answer = []
+    
+    for i in arr:
+        if i not in answer and len(answer) < k:
+            answer.append(i)
+            
+    if len(answer) < k:
+        while(len(answer) != k):
+            answer.append(-1)
+        
+    return answer
